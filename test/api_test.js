862217ca64bd40c11e0c6eb39787cb057d193801
successgilli <successgilli@gmail.com>
2018-11-10 17:20:25 -0800

    added comments to test and edited status codes in the routes [finishes #161863289]

--- test/api_test.js
+++ test/api_test.js
@@ -5,9 +5,11 @@ var expect=require('chai').expect;
 var should=require('chai').should();
 let server=require('../app');
 chai.use(chaiHttp);
+//id and data kept here for reference through out
 var id;
 var data;
-//get parcel
+
+//get parcel rote test
 describe(' test the get parcel route "/"parcels',  function(){
 
 
@@ -27,11 +29,12 @@ let param='/api/v1/parcels';
 
 });
 
-//post test
+//test for post /parcels route
 describe(' test the post parcel route /parcels',  function(){
 
 
 let param='/api/v1/parcels';
+//default values kept in array to be used to simulate multiple post requests
  data=[
   {
     name:'gilbert',
@@ -59,10 +62,11 @@ let param='/api/v1/parcels';
   }
 ];
 
-    it('should respond with a json of what I sent and must generate id for each parcel',(done)=>{
+    it('should respond with status 201 showing order has been created and a json of what I sent and must generate id for each parcel',(done)=>{
+//post each data in array above to the post parcel route and ensure the response is json of the post
       data.forEach(x=>{
           chai.request(server).post(param).send(x).end((err,res)=>{
-            res.should.have.status(200);
+            res.should.have.status(201);
             (res.body).should.be.a('object');
             id=res.body.id;
             (res.body).should.have.property('pickup_location');
@@ -76,16 +80,16 @@ let param='/api/v1/parcels';
 
 });
 
-//get parcel with Parcel  ID
+// test for the get parcel with Parcel  ID
 describe(' test the get route "/parcels/:parcelsId" ',  function(){
 
 
 let param='/api/v1/parcels/:parcelId';
 
-    it('should respond with status 202 if ID not found and return not found message',(done)=>{
+    it('should respond with status 404 if ID not found and return not found message',(done)=>{
 
         chai.request(server).get(param).end((err,res)=>{
-            res.should.have.status(202);
+            res.should.have.status(404);
             (res.body).should.be.a('object');
             res.body.value.should.equal('not found');
 
@@ -96,7 +100,7 @@ let param='/api/v1/parcels/:parcelId';
 
 
     it('should give status of 200 if ID found and return the order data',(done)=>{
-        chai.request(server).get('/api/v1/parcels/' + id).end((err,resp)=>{
+        chai.request(server).get('/api/v1/parcels/'+id).end((err,resp)=>{
           resp.should.have.status(200);
           resp.body.should.be.a('object');
           resp.body.should.have.property('id');
@@ -109,16 +113,16 @@ let param='/api/v1/parcels/:parcelId';
 
 });
 
-//get parcels of a user.
+// test  for the get parcels of a user route.
 describe(' test the get route "/users/:userId/parcels" ',  function(){
 
 
 let param='/api/v1/users/:userId/parcels';
 
-    it('should respond with status 202 if user not found and return a "not found " message',(done)=>{
+    it('should respond with status 404 if user not found and return a "not found " message',(done)=>{
 
         chai.request(server).get(param).end((err,res)=>{
-            res.should.have.status(202);
+            res.should.have.status(404);
             (res.body).should.be.a('object');
             res.body.value.should.equal('not found');
 
@@ -129,14 +133,15 @@ let param='/api/v1/users/:userId/parcels';
 
     it('should return the parcels ordered by user if user id is found',(done)=>{
 
-
+      //randomly select an object/order from the array data above for querry test of userName
       let order=data[Math.floor((Math.random()*5))];
-
+      //querry the user name of the order/object gotten above to see if we get all orders made by the user
       chai.request(server).get('/api/v1/users/'+order.name+'/parcels').end((err,res)=>{
       //  console.log(res.body);
         console.log(order.name);
         res.should.have.status(200);
         res.should.be.a('object');
+        //ensure that we get at least one parcel order since user is expected to be in database
         res.body.should.have.property('0');
       })
       done();
@@ -145,46 +150,47 @@ let param='/api/v1/users/:userId/parcels';
 
 });
 
-//test route get parcels
-describe("testing the route get 'api/parcels' ",function(){
-  it('should be able to produce orders in the database ',(done)=>{
-      chai.request(server).get('/api/v1/parcels').end((err,res)=>{
-        res.body.should.have.property("0");
-        res.should.have.status(200);
-        id=res.body[3].id;
-        res.body.should.be.a('object');
-        for(i=0;i<5;i++){
-            res.body[i].should.have.property('id');
-            res.body[i].should.have.property('name');
-          //  console.log(res.body[i]);
-        }
+//test the route for get parcels to return all orders in database/structure
+  describe("testing the route get 'api/parcels' ",function(){
+    it('should be able to produce orders in the database ',(done)=>{
+        chai.request(server).get('/api/v1/parcels').end((err,res)=>{
+          res.body.should.have.property("0");
+          res.should.have.status(200);
+          id=res.body[3].id;
+          res.body.should.be.a('object');
+          //test that all data returned are valid
+          for(i=0;i<5;i++){
+              res.body[i].should.have.property('id');
+              res.body[i].should.have.property('name');
+            //  console.log(res.body[i]);
+          }
 
 
-      })
-      done();
-  })
-});
+        })
+        done();
+    })
+  });
 
 //test put route '/parcels/:parcelId/cancel'
-describe('testing the put route "api/parcels/:parcelId/cancel" ',function(){
-  let param="api/v1/parcels/:parcelId/cancel";
-  it('should return status 202 if parcelId not found and return a message "not found"',(done)=>{
-    chai.request(server).put(param).end((err,res)=>{
-      res.should.have.status(202);
-      res.body.value.should.equal('not found');
+  describe('testing the put route api/parcels/:parcelId/cancel',function(){
+    let param='/api/v1/parcels/:parcelId/cancel';
+    it('should return status 404 if parcelId not found and return a message "not found"',(done)=>{
+      chai.request(server).put(param).end((err,res)=>{
+        res.should.have.status(404);
+        res.body.value.should.equal('not found');
+      })
+      done();
     })
+  it('should return deleted json showing that data is deleted from database if ID is found',(done)=>{
+
+      console.log('first '+id);
+      chai.request(server).put('/api/v1/parcels/'+id+'/cancel').end((err,res)=>{
+        res.body.should.be.a('object');
+        res.body[0].should.have.property('id');
+        //console.log(res.body);
+      })
     done();
+
+
   })
-it('should return status 200 showing the order has been deleted from the database if ID is found',(done)=>{
-
-    console.log('first '+id);
-    chai.request(server).put('api/v1/parcels/'+id+'/cancel').end((err,res)=>{
-      res.should.have.status(220);
-      res.body.should.be.a('object');
-
-    })
-  done();
-
-
-})
-});
+  });
